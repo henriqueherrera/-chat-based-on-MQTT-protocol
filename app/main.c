58d4fafd541aed56bb4934b7c15754d051b3f7cc
henrique.herrera <henriqueherrera9@gmail.com>
2022-02-08 21:55:37 -0300

    study: library

--- app/main.c
+++ app/main.c
@@ -1,7 +1,82 @@
+#include <signal.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <mosquitto.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-  printf("oioi");
+#define mqtt_host "localhost"
+#define mqtt_port 1883
+
+
+static int run = 1;
+
+void handle_signal(int s)
+{
+	run = 0;
+}
+
+
+void connect_callback(struct mosquitto *mosq, void *obj, int result)
+{
+	printf("connect callback, rc=%d\n", result);
+}
+
+void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+{
+	bool match = 0;
+	printf("Topic: '%s'\nmessage: '%s'\n", message->topic, (char*) message->payload);
+
+	mosquitto_topic_matches_sub("test/messages/+", message->topic, &match);
+	if (match) {
+		printf("got message for ADC topic\n");
+	}
+
+}
+
+int main(int argc, char *argv[])
+{
+	int run = 1;
   
-  return 0;
+	uint8_t reconnect = true;
+	char clientid[24];
+	struct mosquitto *mosq;
+	int rc = 0;
+
+	memset(clientid, 0, 24);
+	printf("What's your client id?\n");
+	scanf("%s", clientid);
+
+	signal(SIGINT, handle_signal);
+	signal(SIGTERM, handle_signal);
+
+	mosquitto_lib_init();
+
+	printf("%s\n",clientid);
+	mosq = mosquitto_new(clientid, true, 0);
+
+	if(mosq)
+	{
+		mosquitto_connect_callback_set(mosq, connect_callback);
+		mosquitto_message_callback_set(mosq, message_callback);
+
+	  rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
+
+		mosquitto_subscribe(mosq, NULL, "test/t2", 0);
+
+		while(run){
+			rc = mosquitto_loop(mosq, -1, 1);
+			if(run && rc){
+				printf("connection error!\n");
+				sleep(10);
+				mosquitto_reconnect(mosq);
+			}
+		}
+		mosquitto_destroy(mosq);
+	}
+
+	mosquitto_lib_cleanup();
+
+	return rc;
 }
